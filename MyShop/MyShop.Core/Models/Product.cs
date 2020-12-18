227ba9949eae4a8c614058372df2c050af97bece
IiD <IndividualsinDemand@gmail.com>
2020-12-18 05:44:15 +0000

    Updated UI

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MyShop.Core.Model
+namespace MyShop.Core.Models
 {
     public class Product
     {
--- MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.Caching;
 using MyShop.Core.Models;
-using MyShop.Core.Model;
 
 namespace MyShop.DataAccess.InMemory
 {
